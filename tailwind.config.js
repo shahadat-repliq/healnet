d717eea6d47b40474304337bb5fcf6d0ff2fd619
shahadat-repliq <shahadathossain.akash@repliq.dev>
2024-04-23 16:38:02 +0600

    project initialized

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,16 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      backgroundColor: {
+        'card-background': 'rgba(62, 207, 151, 0.1)',
+        'get-started': 'rgba(72, 253, 183, 0.3)',
+        'input-field': 'rgba(85, 190, 150, 0.1)',
+        
+      },
+      colors: {
+        'primary-color': '#017A4B',
+        'chip': '#274539'
+      }
     },
   },
   plugins: [],
